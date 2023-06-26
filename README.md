16e8dbbcf558bdfc5960680020fd7afa992f86ee
Pulkit Patel <pulkit2417@gmail.com>
2023-06-26 22:21:18 +0530

    prepare callbacks completed

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 2. update secrets.yaml [Optional]
 3. Update params.yaml
 4. Update the entity
-5. Update the configuration manger in src config
+5. Update the configuration manager in src config
 6. Update the components
 7. Update the pipeline
 8. Update the main.py
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,8 @@
+import os
 from cnnClassifier.costants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
-from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
+
 
 
 class ConfigurationManager:
@@ -46,3 +47,20 @@ class ConfigurationManager:
             params_weights=self.params.WEIGHTS
         )
         return prepare_base_model_config
+    
+
+    def get_prepare_callbacks_config(self) -> PrepareCallbacksConfig:
+          config = self.config.prepare_callbacks
+          model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+          create_directories([
+                Path(model_ckpt_dir),
+                Path(config.tensorboard_root_log_dir)
+          ])
+
+          prepare_callback_config = PrepareCallbacksConfig(
+                root_dir=Path(config.root_dir),
+                tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+                checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+          )
+
+          return prepare_callback_config
